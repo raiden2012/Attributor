4318cc443076749494b9062242c6a02acd218198
Wei Tu <weitu@Weis-MacBook-Pro.local>
2016-01-02 16:58:31 +0800

    Add .gitignore

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -9,10 +9,25 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
 
 @end
 
 @implementation ViewController
+
+- (IBAction)changeBodySelectionColor:(UIButton *)sender {
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName value:sender.backgroundColor range:self.body.selectedRange];
+}
+
+- (IBAction)outlineBodySelection {
+    [self.body.textStorage addAttributes:@{NSStrokeWidthAttributeName : @-3,
+                                           NSStrokeColorAttributeName : [UIColor blackColor]}  range:self.body.selectedRange];
+}
+
+- (IBAction)unoutlineBodySelection {
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName range:self.body.selectedRange];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
