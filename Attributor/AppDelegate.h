76e156dd7c88f1eb1b207c916f8adfac4ec86a1e
wei tu <tuwei18@gmail.com>
2016-01-02 16:27:52 +0800

    Initial Commit

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by Wei Tu on 2/1/2016.
+//  Copyright © 2016 Wei Tu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by Wei Tu on 2/1/2016.
+//  Copyright © 2016 Wei Tu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
